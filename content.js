7d8e9c2c29769146c4d710bef8ea47cfc90aa87e
Philip Trubitsyn <trph@bk.ru>
2022-12-04 19:46:58 +0300

    подготовка расширения

---
+++ content.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let numbers = [];
+let mails = [];
+
+
+
+numbers = 
+
+if (numbers.length == mails.length)
+{
+
+
+
+
+
+
+
+}
+else
+{
+alert("Количество номеров телефона должно совпадать с количеством Email");
+}
